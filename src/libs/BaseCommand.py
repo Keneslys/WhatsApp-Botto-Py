0e9be7fe706d0b769a3a57ae1a9dc5e474a60ebe
Keneslys <160778611+Keneslys@users.noreply.github.com>
2025-07-24 17:08:22 +0300

    Update BaseCommand.py

--- src/libs/BaseCommand.py
+++ src/libs/BaseCommand.py
@@ -1,10 +1,10 @@
-from libs import Void
+from libs import Muzan
 from utils import DynamicConfig
 from libs import MessageClass
 
 
 class BaseCommand:
-    def __init__(self, client: Void, handler, config: dict):
+    def __init__(self, client: Muzan, handler, config: dict):
         self.client = client
         self.handler = handler
         self.config = DynamicConfig(config)
